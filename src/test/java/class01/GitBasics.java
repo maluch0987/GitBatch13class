e5b3c1f95dc61065cf19e1592dc92c208654b01e
malush0987 <qa0150783@gmail.com>
2022-08-24 21:00:45 -0400

    first

--- src/test/java/class01/GitBasics.java
+++ src/test/java/class01/GitBasics.java
@@ -4,6 +4,6 @@ public class GitBasics {
     public static void main(String[] args) {
         //write down a code that checks the temperature from a website and then
         //prints out that is it a beautiful day or not
-        System.out.println("Today is beautiful day");
+        System.out.println("Today is not beautiful day");
     }
 }
